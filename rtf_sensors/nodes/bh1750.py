e0d560b4cc4549fb88e1561c7af47dd63075387b
Kevin <walchko@users.noreply.github.com>
2020-08-30 16:55:32 -0400

    Create bh1750.py

---
+++ rtf_sensors/nodes/bh1750.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2020 Kevin Walchko
+# see LICENSE for full details
+##############################################
